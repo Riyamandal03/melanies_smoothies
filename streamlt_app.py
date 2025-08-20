417890c2c0b8935ba5463bf5e9b7eab94ba4aebd
Riyamandal03 <riya.mandal@in.abb.com>
2025-08-20 13:31:18 +0530

    Update streamlt_app.py

--- streamlt_app.py
+++ streamlt_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
 
         # ✅ Get the search_on value from the DataFrame
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         # Show nutrition info
         st.subheader(fruit_chosen + ' Nutrition Information')

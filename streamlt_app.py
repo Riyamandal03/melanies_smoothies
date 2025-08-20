5ec00704f177f96e6f1292b1def4188f3d750e95
Riyamandal03 <riya.mandal@in.abb.com>
2025-08-20 11:34:34 +0530

    Update streamlt_app.py

--- streamlt_app.py
+++ streamlt_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from  snowflake.snowpark.functions import col
 import pandas as pd
+import requests
  
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
@@ -26,6 +27,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + '  '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string)
  
     my_insert_stmt = f"""
@@ -41,10 +45,10 @@ VALUES ('{ingredients_string}', '{name_on_order}')
         st.success('Your Smoothie is ordered!', icon="✅")
  
 # New section to display smoothiefroot nutrition information 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+
+
 #st.text(smoothiefroot_response.json())
 df = pd.DataFrame(smoothiefroot_response.json())
 st.dataframe(df, use_container_width=True)
-st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+#st.text(smoothiefroot_response.json())
+
